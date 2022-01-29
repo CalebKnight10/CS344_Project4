6b5cb54a8fc8d580ada43bcbf4fe3d32e58eebe7
Caleb Knight <knightca@oregonstate.edu>
2022-01-29 09:33:04 -0800

    Implemented lswc and fixed wc error

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # CS344_Project4
-# Caleb Knight
-# Project 4
-# CS 344
-# An exercise to pipe the output of a process to the input of another
-# and save the output in a file
-# Goal: run ls -1a and wc -l --> pipe the output of ls into wc
-# % ls -1a | wc -l   --> Tells us how many files in curr dir
+ Caleb Knight
+ Project 4
+ CS 344
+ An exercise to pipe the output of a process to the input of another
+ and save the output in a file
+ Goal: run ls -1a and wc -l --> pipe the output of ls into wc
+ % ls -1a | wc -l   --> Tells us how many files in curr dir
--- lswc.c
+++ lswc.c
@@ -1,10 +1,7 @@
 // Caleb Knight
 // Project 4
 // CS 344
-// An exercise to pipe the output of a process to the input of another
-// and save the output in a file
-// Goal: run ls -1a and wc -l --> pipe the output of ls into wc
-// % ls -1a | wc -l   --> Tells us how many files in curr dir
+
 
 
 #include <stdio.h>   // printf perror
@@ -13,36 +10,25 @@
 
 int main(int argc, char *argv[]) 
 {
-	int fds[2];   // 0 is read end, 1 is write end
 
-	//char *input[];
+	// if(argc !=2) {
+	// 	perror("Wrong number of commands (Expects 2)");
+	// }
 
-	pid_t pid = fork();
+	int fds[2];   // File desciptors, 0 is read end, 1 is write end
+	pipe(fds);   // Create pipe
+	int pid = fork();
 
 	if(pid == 0) {
-		int result;
-		printf("Child's running\n");
-
-		close(fds[1]);
-		read(fds[0], &result, sizeof result);
-
-		printf("child: %d\n", result);
-
+		dup2(fds[0], 0); // Dup read end 
+		close(fds[1]);   // Close write end
+		execlp("wc", "wc", "-l", (char *) NULL);
 		exit(0);
 	}
 
-	printf("Parent's running\n");
-	close(fds[0]);
-	execlp("ls", "ls", "-1a", argv[1], NULL);
-
-
-
-	printf("0: %s\n", argv[0]);          // prints args from command line 
-	for(int i = 1; i < argc; i ++)       // with corresponding number 
-	{                                    // based on order of args entered
-		printf("%d: %s\n", i, argv[i]);
-	}
-
+	dup2(fds[1], 1);     // Dup write end of pipe to stdout
+	close(fds[0]);       // close read end
+	execlp("ls", "ls", "-1a", argv[1], (char *) NULL);
 }
 
 
