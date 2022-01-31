52dcc55add9c02826b2e7a66b5357a5bd119c06c
Caleb Knight <knightca@oregonstate.edu>
2022-01-31 10:37:21 -0800

    Cleaned up lswc

--- lswc.c
+++ lswc.c
@@ -1,11 +1,30 @@
 // Caleb Knight
 // CS 344
 // Project 4
-// Lswc
+// ls | wc
 
 #include <stdio.h>   // printf perror
 #include <stdlib.h>  // exit
 #include <unistd.h>  // fork execlp close
+
+
+void child_process(int pid, int fds[]) 
+{
+	if(pid == 0) {
+		dup2(fds[0], 0); // Dup read end 
+		close(fds[1]);   // Close write end
+		execlp("wc", "wc", "-l", (char *) NULL);
+		exit(0);
+	}
+}
+
+void parent_process(int fds[], char *argv[]) 
+{
+	dup2(fds[1], 1);     // Dup write end of pipe to stdout
+	close(fds[0]);       // close read end
+	execlp("ls", "ls", "-1a", argv[1], (char *) NULL);	
+}
+
 
 int main(int argc, char *argv[]) 
 {
@@ -13,19 +32,9 @@ int main(int argc, char *argv[])
 	pipe(fds);   // Create pipe
 	int pid = fork(); // used to fork the parent and get a child
 
+	child_process(pid, fds);
 
-	// child stuff
-	if(pid == 0) {
-		dup2(fds[0], 0); // Dup read end 
-		close(fds[1]);   // Close write end
-		execlp("wc", "wc", "-l", (char *) NULL);
-		exit(0);
-	}
-
-	// parent stuff
-	dup2(fds[1], 1);     // Dup write end of pipe to stdout
-	close(fds[0]);       // close read end
-	execlp("ls", "ls", "-1a", argv[1], (char *) NULL);
+	parent_process(fds, argv);
 }
 
 
